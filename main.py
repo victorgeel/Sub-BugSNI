279e10fb2ae12bfa727c6a848709a9ff19479416
Victor is Geek <92975790+victorgeel@users.noreply.github.com>
2024-06-10 08:17:01 +0630

    Update main.py
    Signed-off-by: Victor is Geek <92975790+victorgeel@users.noreply.github.com>

--- main.py
+++ main.py
@@ -1,24 +1,31 @@
-import ipaddress, requests
+import ipaddress
+import requests
 from multiprocessing import Process
-import time, random
+import time
+import random
+from termcolor import colored
 
-ip_ranges = [line.strip() for line in open('ip.txt', 'r').readlines()] #IP ranges
+ip_ranges = [line.strip() for line in open('ip.txt', 'r').readlines()]  # IP ranges
 
 def range_test(network, ping):
     for ip in network:
         try:
             start_time = time.time()
-            result = requests.get(f"https://{ip}/__down", timeout=(10,10))
+            result = requests.get(f"https://{ip}/__down", timeout=(10, 10))
             end_time = time.time()
             ping_time = round((end_time - start_time) * 1000, 2)
             if int(ping_time) <= ping:
-                print(f"Clean IP : {ip} (Ping: {ping_time} ms)")
+                ip_text = colored(f"Clean IP : {ip}", "green")
+                ping_text = colored(f"(Ping: {ping_time} ms)", "yellow")
+                print(f"{ip_text} {ping_text}")
         except Exception as e:
             if "CERTIFICATE_VERIFY_FAILED" in str(e):
                 end_time = time.time()
                 ping_time = round((end_time - start_time) * 1000, 2)
                 if int(ping_time) <= ping:
-                    print(f"Clean IP : {ip} (Ping: {ping_time} ms)")
+                    ip_text = colored(f"Clean IP : {ip}", "green")
+                    ping_text = colored(f"(Ping: {ping_time} ms)", "yellow")
+                    print(f"{ip_text} {ping_text}")
 
 if __name__ == '__main__':
     ping = int(input('Enter the maximum delay recommend 3000(Ping) ms <= 10000 : '))
@@ -28,3 +35,4 @@ if __name__ == '__main__':
         print(ip_range, 'Started !')
         network = ipaddress.ip_network(ip_range)
         Process(target=range_test, args=(network, ping)).start()
+                
